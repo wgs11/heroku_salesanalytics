c70861e6ff1c2625c25dbafeb7a717a0233c8d90
wgs11 <4teatoo@gmail.com>
2019-01-12 22:19:33 -0500

    more stuff

--- store.go
+++ store.go
@@ -34,7 +34,7 @@ func (store *dbStore) CheckUser(creds *Credentials) error {
 
 func (store *dbStore) CreateUser(creds *NewUser) error {
 	hashedPassword,_ := bcrypt.GenerateFromPassword([]byte(creds.Password),8)
-	_,err := store.db.Query("INSERT INTO employees(fname,lname,position,store_id,user_name,password) VALUES ($1, $2, $3, $4, $5, $6)", string(creds.First), string(creds.Last), creds.Position, 3, string(creds.Username), string(hashedPassword))
+	_,err := store.db.Query("INSERT INTO employees(fname,lname,position,store_id,user_name,password) VALUES ($1, $2, $3, $4, $5, $6)", string(creds.First), string(creds.Last), 2, 3, string(creds.Username), string(hashedPassword))
 	if err != nil {
 		fmt.Println(err)
 		return err
